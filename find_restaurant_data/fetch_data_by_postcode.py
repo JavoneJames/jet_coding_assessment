4f88aff4b70f2b786153545a10d7f3343fe42085
jovane <javonejames@icloud.com>
2026-03-25 03:40:13 +0000

    chore: add schema for core methods to be implemented

---
+++ find_restaurant_data/fetch_data_by_postcode.py
@@ -0,0 +1,14 @@
+import requests
+
+# send a 'get' requests ot the api using a postcode
+def get_postcode_data():
+    pass
+
+# filter the received data to focus on restaurant data
+def filter_received_data():
+    pass
+
+# from these restaurant object extract Name, Cuisines, Rating -as a number, and Address
+def extract_relevant_data_points():
+    pass
+
