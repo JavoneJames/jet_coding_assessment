10569fd3a524f448dafac6e887e7cdb1bfe04524
jovane <javonejames@icloud.com>
2026-03-25 06:39:31 +0000

    fix: minor fix resolved by adding init file to exec test script using pytest

--- tests/test_fetch_data.py
+++ tests/test_fetch_data.py
@@ -1,7 +1,8 @@
 import pytest # type: ignore
-from find_restaurant_data.fetch_data_by_postcode import get_postcode_data 
+# from find_restaurant_data.fetch_data_by_postcode import get_postcode_data 
+from find_restaurant_data.fetch_data_by_postcode import get_postcode_data
+
 
 def test():
     print(pytest.__version__)
 
-test()
