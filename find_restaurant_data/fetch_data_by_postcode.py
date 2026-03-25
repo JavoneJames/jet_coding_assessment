0cb621bde83d4bc5b3803f52dd0085174e7c8caf
jovane <javonejames@icloud.com>
2026-03-25 11:26:02 +0000

    feat: get_postcode_data method invokes validate postcode method then makes get req using url and postcode, then gets the status code of the res obj

--- find_restaurant_data/fetch_data_by_postcode.py
+++ find_restaurant_data/fetch_data_by_postcode.py
@@ -1,6 +1,8 @@
 import requests
 import re
+
 POST_CODE = "EC4M7RF"
+API_URL = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/"
 
 # check if the postcode is valid based on UK format
 def validate_postcode(postcode: str) -> bool:
@@ -14,14 +16,20 @@ def validate_postcode(postcode: str) -> bool:
         r"\s?[0-9][ABD-HJLNP-UW-Z]{2}$"                    # Inward code
     )    
     return bool(re.match(pattern=pattern, string=postcode))
-# send a 'get' requests ot the api using a postcode
-def get_postcode_data():
-    pass
 
 # check response status code
-def check_response_status_code():
-    pass
-
+def check_response_status_code(status):
+   return status == 200 
+# send a 'get' requests ot the api using a postcode
+def get_postcode_data(url: str, postcode: str):
+    if not validate_postcode(postcode):
+        raise ValueError(f"Invalid postcode: {postcode}")
+    try:
+        response = requests.get(f"{url}{postcode}")
+        if not check_response_status_code(response.status_code):
+            raise RuntimeError(f"Invalid status code: {response.status_code}")
+    except requests.RequestException as e:
+        raise Exception(f"API request failed: {e}")
 # filter the received data to focus on restaurant data
 def filter_received_data():
     pass
@@ -30,4 +38,6 @@ def filter_received_data():
 def extract_relevant_data_points():
     pass
 
-print(validate_postcode(POST_CODE))
+get_postcode_data(API_URL, POST_CODE)
+
+# print(validate_postcode(POST_CODE))
