403b21e34820fccc5adada62b0251fd51ed7c231
jovane <javonejames@icloud.com>
2026-03-26 01:19:09 +0000

    test: add mock unit tst for get_postcode_data

--- tests/test_fetch_data_by_postcode.py
+++ tests/test_fetch_data_by_postcode.py
@@ -1,6 +1,28 @@
-import pytest # type: ignore
-# from find_restaurant_data.fetch_data_by_postcode import get_postcode_data 
-from find_restaurant_data.fetch_data_by_postcode import validate_postcode, get_postcode_data, check_response_status_code, filter_received_data, extract_relevant_data_points
+import requests
+import pytest  #type: ignore
+from unittest.mock import patch, MagicMock
+
+from find_restaurant_data.fetch_data_by_postcode import (
+    validate_postcode,
+    get_postcode_data,
+    check_response_status_code,
+    filter_received_data,
+    extract_relevant_data_points,
+)
+
+API_URL = "http://fakeurl.com/"
+POST_CODE = "SE1 9AD"
+USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Mobile Safari/537.36"
+headers = {"User-Agent": USER_AGENT}
+RECEIVED_DATA = {
+    "metaData": {},
+    "restaurants": [],
+    "deliveryFees": {},
+    "promotedPlacement": [],
+    "filters": {},
+    "layout": {},
+    "enrichedLists": [],
+}
 
 def test_validate_postcode():
     assert validate_postcode("EC4M7RF") == True
@@ -16,14 +38,27 @@ def test_check_response_status_code():
     assert check_response_status_code(408) is not True
 
 def test_get_postcode_data():
-    pass
+    with patch("find_restaurant_data.fetch_data_by_postcode.validate_postcode", return_value=True), \
+        patch("find_restaurant_data.fetch_data_by_postcode.check_response_status_code", return_value=True), \
+        patch("find_restaurant_data.fetch_data_by_postcode.requests.get") as mock_get:
+
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = RECEIVED_DATA
+        mock_get.return_value = mock_response
+        
+        data = get_postcode_data(API_URL, POST_CODE)
+        mock_get.assert_called_once_with(
+            url=f"{API_URL}{POST_CODE}",
+            headers=headers  
+        )
+        
+        expected_keys = ['metaData', 'restaurants', 'deliveryFees', 'promotedPlacement', 'filters', 'layout', 'enrichedLists']
+        assert set(data.keys()) == set(expected_keys)
 
 def test_filter_received_data():
-    pass
+    pass 
 
 def test_extract_relevant_data_points():
     pass
-
-# def test():
-#     print(pytest.__version__)
 
