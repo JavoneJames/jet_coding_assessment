917995140eac3b2f9a6e7335cb3bd9ea0dc7061d
jovane <javonejames@icloud.com>
2026-03-28 11:02:32 +0000

    fix: use pydantic.RootModel to define root model instead of __root__

--- backend/models/restaurant_data_model.py
+++ backend/models/restaurant_data_model.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 from typing import List, Dict
 
+from pydantic.root_model import RootModel
+
 class RestaurantModel(BaseModel):
     name: str
     cuisines: List[str]
     rating: float
     address: str
 
-class RestaurantData(BaseModel):
-    __root__: Dict[int, RestaurantModel]
+class RestaurantData(RootModel):
+    Dict[int, RestaurantModel]
