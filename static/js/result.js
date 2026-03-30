1309a990eadfc23e73a8858a8901a149f1bc3264
jovane <javonejames@icloud.com>
2026-03-30 19:17:24 +0100

    refac: refactor and modularise methods for clarity

--- static/js/result.js
+++ static/js/result.js
@@ -20,17 +20,15 @@ function renderRestaurants(fetchedRestaurants) {
 
   resultsDiv.innerHTML = "";
 
-  if (Object.keys(fetchedRestaurants).length === 0) {
+  if (fetchedRestaurants.length === 0) {
     displayError.textContent = "No restaurants found.";
     return;
   }
 
-  Object.keys(fetchedRestaurants).forEach((key) => {
-    const restaurant = fetchedRestaurants[key];
+  Object.values(fetchedRestaurants).forEach((restaurant) => {
     const clone = template.content.cloneNode(true);
     clone.querySelector(".name").textContent = restaurant.name;
-    clone.querySelector(".cuisines").textContent =
-      restaurant.cuisines.join(", ");
+    clone.querySelector(".cuisines").textContent = restaurant.cuisines.join(", ");
     clone.querySelector(".rating").textContent = restaurant.rating;
     clone.querySelector(".address").textContent = restaurant.address;
     resultsDiv.appendChild(clone);
@@ -38,12 +36,10 @@ function renderRestaurants(fetchedRestaurants) {
 }
 
 function filterSearchResults(fetchedRestaurants, search) {
-  if (!fetchedRestaurants) return restaurants;
+  if (!fetchedRestaurants) return fetchedRestaurants;
 
   return Object.values(fetchedRestaurants).filter((restaurant) => {
-    const nameMatch = restaurant.name
-      .toLowerCase()
-      .includes(search.toLowerCase());
+    const nameMatch = restaurant.name.toLowerCase().includes(search.toLowerCase());
     const cuisineMatch = restaurant.cuisines
       .join(", ")
       .toLowerCase()
@@ -52,60 +48,61 @@ function filterSearchResults(fetchedRestaurants, search) {
   });
 }
 
-function searchHandler(fetchedRestaurants) {
+function renderFilteredRestaurants(fetchedRestaurants, filteredResults) {
+  const displayError = document.getElementById("display-error");
+  if (filteredResults.length > 0) {
+    displayError.textContent = "";
+    renderRestaurants(filteredResults);
+  } else {
+    displayError.textContent = "No restaurants found.";
+    document.getElementById("results").innerHTML = "";
+  }
+}
+
+function searchHandler(fetchedRestaurants = []) {
   const searchInput = document.getElementById("search-bar");
 
   searchInput.addEventListener("input", () => {
     const search = searchInput.value;
     const filteredResults = filterSearchResults(fetchedRestaurants, search);
 
-    if (filteredResults && filteredResults.length > 0) {
-      renderRestaurants(filteredResults);
-    } else {
-      renderRestaurants(fetchedRestaurants);
-    }
+    renderFilteredRestaurants(fetchedRestaurants, filteredResults);
   });
 }
 
 function ratingHandler(fetchedRestaurants) {
   const ratingContainer = document.getElementById("rating-options");
   if (!ratingContainer) return;
+
   const inputs = ratingContainer.getElementsByTagName("input");
-  const checkboxes = Array.from(inputs).filter(
-    (input) => input.type === "checkbox",
-  );
+  const checkboxes = Array.from(inputs).filter((input) => input.type === "checkbox");
 
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
-      const selectedRatings = checkboxes
-        .filter((cb) => cb.checked)
-        .map((cb) => Number(cb.value));
-      console.log("Selected ratings:", selectedRatings);
-      const filteredByRating = Object.values(fetchedRestaurants).filter(
-        (restaurant) => {
-          const roundRating = Math.round(restaurant.rating);
-          return (
-            selectedRatings.length === 0 ||
-            selectedRatings.includes(roundRating)
-          );
-        },
+      const selectedRatings = getSelectedRatings(checkboxes);
+      const filteredRestaurantsByRating = getFilteredRestaurantsByRating(
+        fetchedRestaurants,
+        selectedRatings,
       );
-
-      const displayError = document.getElementById("display-error");
-      if (filteredByRating.length > 0) {
-        displayError.textContent = "";
-        renderRestaurants(filteredByRating);
-      } else {
-        displayError.textContent =
-          "No restaurants found for selected rating(s).";
-        document.getElementById("results").innerHTML = "";
-      }
+      renderFilteredRestaurants(fetchedRestaurants, filteredRestaurantsByRating);
     });
+  });
+}
+
+function getSelectedRatings(checkboxes) {
+  return checkboxes.filter((cb) => cb.checked).map((cb) => Number(cb.value));
+}
+
+function getFilteredRestaurantsByRating(fetchedRestaurants, selectedRatings) {
+  return Object.values(fetchedRestaurants).filter((restaurant) => {
+    const roundRating = Math.round(restaurant.rating);
+    return selectedRatings.length === 0 || selectedRatings.includes(roundRating);
   });
 }
 
 async function initialize() {
   const fetchedRestaurants = await fetchRestaurantsFromServer();
+  if (Object.keys(fetchedRestaurants).length === 0) return;
   renderRestaurants(fetchedRestaurants);
   searchHandler(fetchedRestaurants);
   ratingHandler(fetchedRestaurants);
