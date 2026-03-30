19bfe6c22fa3aa2655555065f358fa4c3566b1c2
jovane <javonejames@icloud.com>
2026-03-30 23:59:21 +0100

    refac(fix): removed unused code and add minor modularity

--- static/js/button.js
+++ static/js/button.js
@@ -1,31 +1,28 @@
-const button = document.getElementById("search-button");
-const inputText = document.getElementById("search-bar");
-const displayError = document.getElementById("display-error");
+function getUserInput() {
+  const displayError = document.getElementById("display-error");
+  const inputText = document.getElementById("search-bar");
+  const userInput = inputText.value.trim();
+  return userInput;
+}
 
-button.addEventListener("click", async () => {
-  try {
-    const userInput = inputText.value.trim();
-    if (!userInput) {
-      displayError.textContent = "Please enter a postcode";
-      console.log("no input");
-      return;
-    }
-    displayError.textContent = "";
-    const res = await fetch(
-      `http://127.0.0.1:8000/restaurants/${encodeURIComponent(userInput)}`,
-    );
-    if (!res.ok) throw new Error(`Server error: ${res.status}`);
-    const data = await res.json();
-    const restaurants = Object.values(data);
+function buttonHandler() {
+  const button = document.getElementById("search-button");
 
-    if (restaurants.length == 0) {
-      displayError.textContent = `No restaurants found near ${userInput}`;
-    } else {
-      document.getElementById("display-error").textContent = "";
+  button.addEventListener("click", async () => {
+    const displayError = document.getElementById("display-error");
+    try {
+      const userInput = getUserInput();
+      if (!userInput) {
+        displayError.textContent = "Please enter a postcode";
+      } else {
+        displayError.textContent = "";
+        window.location.href = `/frontend/results.html?postcode=${encodeURIComponent(userInput)}`;
+      }
+    } catch (err) {
+      console.error("error", err);
+      displayError.textContent = err.message;
     }
-    window.location.href = `/frontend/results.html?postcode=${encodeURIComponent(userInput)}`;
-  } catch (err) {
-    console.error("error", err);
-    displayError.textContent = err.message;
-  }
-});
+  });
+}
+
+buttonHandler();
