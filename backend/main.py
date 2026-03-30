00180ac08bc70e61cdfe955d655f2b6751dc26c9
jovane <javonejames@icloud.com>
2026-03-30 21:33:31 +0100

    refac: removed unused routes and functions

--- backend/main.py
+++ backend/main.py
@@ -33,19 +33,3 @@ def get_restaurants(postcode: Annotated[str, Depends(validate_postcode)]):
         return restaurants_data
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-
-@app.get("/restaurants/cuisines_types")
-def get_by_cuisines(post_code: str, cuisine_type: str):
-    received_data = pc_api.get_postcode_data(post_code)
-    filtered_data = pc_api.filter_received_data(received_data)
-    restaurants_data = pc_api.extract_relevant_data_points(
-        filtered_data, cuisine_type=cuisine_type
-    )
-    return restaurants_data
-
-@app.get("/restaurants/ratings")
-def get_by_ratings(post_code: str, rating: int):
-    received_data = pc_api.get_postcode_data(post_code)
-    filtered_data = pc_api.filter_received_data(received_data)
-    restaurants_data = pc_api.extract_relevant_data_points(filtered_data, rating=rating)
-    return restaurants_data
